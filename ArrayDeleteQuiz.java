d4a41829640cd78f7c2ac55e12be73ed8a0f68e8
weirdocode <92239346+weirdocode@users.noreply.github.com>
2021-10-27 23:45:05 +0900

    Day07

--- ArrayDeleteQuiz.java
+++ ArrayDeleteQuiz.java
@@ -17,13 +17,30 @@ public class ArrayDeleteQuiz {
 		 2. 입력받은 이름이 없다면 없다고 얘기해 주세요.
 		 
 		 */
-		
+		boolean flag = false;
 		for(int i=0; i<kakao.length; i++) {
 			if (name.equals(kakao)) {
-				
+				flag = true;
+				for (int j=i; j<kakao.length-1; j++) {
+					kakao[j] = kakao[j+1];
+				}
+				break;
 			}
+			
 		}
 		
+		if (!flag) {// flag ==false?
+			System.out.println(name + "은(는) 없는 이름입니다");
+		}else {
+			String[] temp = new String[kakao.length-1];
+			for(int k=0; k<temp.length;k++) {
+				temp[k] = kakao[k];
+			}
+			kakao = temp;
+			temp = null;
+			
+			System.out.println("삭제 후 정보: " + Arrays.toString(kakao));
+		}
 		
 		
 		
